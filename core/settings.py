b2ae1bcf6bad9cc3f11e624d5ba247f7d7ef07fa
BwanaQ <thunjawax@gmail.com>
2021-12-07 14:49:18 +0300

    crsf fixes

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['crud-store-2.herokuapp.com']
+
+
+CSRF_TRUSTED_ORIGINS = ['https://crud-store-2.herokuapp.com']
 
 
 # Application definition
