91321f987f9c8035bcaed58b18c3948e54232e9a
BwanaQ <thunjawax@gmail.com>
2021-12-07 15:03:33 +0300

    add allowd localhost to allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -25,10 +25,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['crud-store-2.herokuapp.com']
+ALLOWED_HOSTS = ['crud-store-2.herokuapp.com','localhost','127.0.0.1']
 
 
-CSRF_TRUSTED_ORIGINS = ['https://crud-store-2.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = ['https://crud-store-2.herokuapp.com','http://localhost:8000','http://127.0.0.1:8000']
 
 
 # Application definition
