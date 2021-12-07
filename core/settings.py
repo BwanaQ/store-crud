20f8c2fb086e9d878b549e138acfafcb0e718bb1
BwanaQ <thunjawax@gmail.com>
2021-12-07 14:33:18 +0300

    add django_heroku import

--- core/settings.py
+++ core/settings.py
@@ -152,3 +152,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+import django_heroku
+django_heroku.settings(locals())
